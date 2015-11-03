af1f63df88d5ab1d32cf9a39443f35f86a7e2aec
Youngchangoon <youngchangoon521@gmail.com>
2015-11-03 15:46:28 +0900

    sex

---
+++ PangGame_new/Assets/Scripts/InGame/Manager/GameCore.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum BlockColor
+{
+    None = 0,
+    Red,
+    Yellow,
+    Green,
+    Blue,
+    Purple
+};
+
+public class GameCore : Singleton<GameCore> 
+{
+    public int[,] board = new int[7,6];
+
+
+    [HideInInspector] public bool isSelected;  // 눌린지 안눌린지 판단
+    [HideInInspector] public BlockColor selectedColor;  // 같은색깔만 해야되니까
+    [HideInInspector] public List<NormalBlock> selectedBlock = new List<NormalBlock>(); // 다모으고 터뜨리기위해
+    [HideInInspector] public NormalBlock lastBlock;
+    Vector2 touchPos;
+    RaycastHit2D hitInfo;
+
+    void Awake()
+    {
+        isSelected = false;
+        selectedColor = BlockColor.None;
+    }
+
+    void Update()
+    {
+
+    }
+
+
+
+    public void AddRow(BlockColor blockColor, int generatorNumber,int currBlockIdx)
+    {
+        // 2가지 방법이있다 1. 왼쪽아래가 0,0 인방법 2. 왼쪽위가 0,0 인 방법 
+        // 지금은 2번을 씀
+        board[6-currBlockIdx, generatorNumber] = (int)blockColor;
+    }
+}
+
---
+++ PangGame_new/Assets/Scripts/InGame/Object/Object/NormalBlock.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class NormalBlock : MonoBehaviour
+{
+    public Sprite[] colorSprite;
+    BlockGenerator gene;
+    GameCore core;
+    NormalBlock currBlock;
+
+    int randomColor;
+    BlockColor blockColor;
+
+    void Awake()
+    {
+        gene = transform.parent.GetComponent<BlockGenerator>();
+        core = GameObject.FindObjectOfType<GameCore>();
+    }
+
+
+    public void InitWithGenerator(BlockGenerator gene)
+    {
+        // 0은 None으로 두고 1부터 5까지 색깔 
+        randomColor = Random.Range(1, 6);
+        blockColor = (BlockColor)randomColor;
+        
+        GetComponent<SpriteRenderer>().sprite = colorSprite[randomColor-1];
+
+        transform.position = gene.transform.position;
+        gameObject.SetActive(true);
+    }
+
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector2(gene.transform.position.x, transform.position.y);
+    }
+
+
+
+    public BlockColor GetColor()
+    {
+        return blockColor;
+    }
+
+
+    public void OnDisable()
+    {
+        gene.currBlockCount--;
+    }
+
+
+    // 터치 관련 부분
+    void OnMouseDown()
+    {
+        if (core.isSelected)
+            return;
+
+        Debug.Log("DOWN");
+        core.isSelected = true;
+        core.selectedColor = blockColor;
+        core.selectedBlock.Add(this);
+        core.lastBlock = this;
+        
+    }
+
+    void OnMouseEnter()
+    {
+        if (!core.isSelected)
+            return;
+
+        // 내가 갔었던 곳인지
+        for (int i = 0; i < core.selectedBlock.Count; i++)
+        {
+            if (core.selectedBlock[i] == this)
+                return;
+        }
+
+        if(core.selectedColor == blockColor)
+        {
+            if(Mathf.Abs(this.transform.position.x - core.lastBlock.transform.position.x) < 1.2f &&
+                Mathf.Abs(this.transform.position.y - core.lastBlock.transform.position.y) < 1.3f) 
+            {
+                // 조건에 다 맞는것들 여기서 처리
+                core.selectedBlock.Add(this);
+                core.lastBlock = this;
+                return;
+            }
+        }
+
+    }
+
+    void OnMouseUp()
+    {
+        
+        core.isSelected = false;
+
+        if (core.selectedBlock.Count < 3)
+        {
+            core.selectedBlock.Clear();
+            return;
+        }
+
+
+        for (int i = 0; i < core.selectedBlock.Count; i++)
+        {
+            core.selectedBlock[i].gameObject.SetActive(false);
+            core.lastBlock = null;
+        }
+
+        core.selectedBlock.Clear();
+        core.selectedColor = BlockColor.None;
+
+
+    }
+
+
+
+
+
+}
