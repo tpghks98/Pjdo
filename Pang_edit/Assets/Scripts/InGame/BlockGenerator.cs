2f50bc80eed117e28fc4b3ec26ea30f3a5ab4f1d
Youngchangoon <youngchangoon521@gmail.com>
2015-11-29 00:11:19 +0900

    a

---
+++ Pang_edit/Assets/Scripts/InGame/BlockGenerator.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockGenerator : MonoBehaviour
+{
+    public int generatorIdx; // 보드판 idx
+
+    private int currBlockCount = 0;
+    private int blockMaxCount  = 7;
+    private Transform temp;
+    private InGameData data;
+    private int sameInterval = 6;
+    private int currIdx = 1;
+    private int prevRand = 0;
+
+    void Awake()
+    {
+        data = GameObject.FindObjectOfType<InGameData>();
+    }
+    void Start()
+    {
+        StartCoroutine(LineCheck());
+        
+    }
+
+    IEnumerator LineCheck()
+    {
+        while (true)
+        {
+            if(currBlockCount < blockMaxCount)
+            {
+
+
+                temp = FIndUnActive();
+                int rand = Random.Range(1, 6);
+                if(currIdx == sameInterval)
+                {
+                    Debug.Log("SE");
+                    rand = prevRand;
+                    currIdx = 0;
+                }
+                temp.GetComponent<NormalBlock>().InitWithGenerator(this, generatorIdx, 6 - currBlockCount, rand);
+
+                data.board[generatorIdx, 6 - currBlockCount] = 
+                    temp.GetComponent<NormalBlock>();
+
+                currIdx++;
+                prevRand = rand;
+                ++currBlockCount;
+                yield return new WaitForSeconds(0.1f);
+            }
+            yield return null;
+        }
+    }
+
+
+    public void DownBlock(int pivot)
+    {
+        NormalBlock[] temps = transform.GetComponentsInChildren<NormalBlock>();
+
+        for (int i = pivot; i >0; i--)
+        {
+            data.board[generatorIdx, i] = data.board[generatorIdx, i - 1];
+        }
+        for (int i = 0; i < temps.Length; i++)
+        {
+            if (temps[i]._blockY < pivot)
+                temps[i]._blockY++;
+        }
+    }
+    public void MinusCurrBlockCount(int minusValue)
+    {
+        if (currBlockCount - minusValue <= 0)
+            currBlockCount = 0;
+        else
+            currBlockCount -= minusValue;
+    }
+
+
+
+    Transform FIndUnActive()
+    {
+        Transform [] tempGO = transform.GetComponentsInChildren<Transform>(true);
+
+        for (int i = 1; i < tempGO.Length; i += 2)
+        {
+            if(!tempGO[i].gameObject.activeSelf)
+            {
+                return tempGO[i];
+            }
+        }
+        // Empty
+        Debug.LogError("None Block");
+        return null;
+    }
+}
---
+++ Pang_edit/Assets/Scripts/InGame/Manager/InGameData.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum BlockColor
+{
+    None = 0,
+    Red,
+    Yellow,
+    Green,
+    Blue,
+    Purple,
+    Gray
+};
+
+
+public class InGameData : MonoBehaviour
+{
+    public List<NormalBlock> selectedBlock = new List<NormalBlock>();
+
+    public bool isStart = false;
+    public int score = 0;
+    public bool isPause = false;
+    public int maxPoint = 100;
+    public float[] point = new float[5];
+    public NormalBlock[,] board = new NormalBlock[6, 7];
+
+
+    private NormalBlock baseBlock;
+    void Awake()
+    {
+
+        Debug.Log("Data Awake");
+    }
+
+
+    public void AddScore(int add)
+    {
+        score += add;
+    }
+
+    public void AddPoint(int add,BlockColor color)
+    {
+        if (point[(int)color - 1] + add < maxPoint)
+            point[(int)color - 1] += add;
+        else
+            point[(int)color - 1] = maxPoint;
+    }
+
+    public void StartCheck()
+    {
+        baseBlock = selectedBlock[0];
+        CheckAround(baseBlock._blockX, baseBlock._blockY);
+
+        if (selectedBlock.Count > 1)
+        {
+            Debug.Log("PANG");
+            FindObjectOfType<Character>().Pang();
+        }
+        else
+            FindObjectOfType<Character>().Mistake();
+
+
+        for (int i = 0; i < selectedBlock.Count; i++)
+        {
+
+            // check 풀기
+            selectedBlock[i].isChecked = false;
+            // 터지는 조건 
+            if (selectedBlock.Count > 1)
+            {
+                selectedBlock[i].transform.GetChild(0).gameObject.SetActive(true);
+                // 점수추가 해야됨 
+                AddPoint(1, selectedBlock[i].GetBlockColor());
+                AddScore(10);
+            }
+            else
+            {
+                // 틀렸을때
+                selectedBlock[i].ChangeColor(BlockColor.Gray);
+                AddPoint(-3, selectedBlock[i].GetBlockColor()); 
+                AddScore(-10);
+            }
+
+        }
+
+        selectedBlock.Clear();
+    }
+    
+    // 상하좌우 순
+    private void CheckAround(int x,int y)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    if (IsSameColor(x, y - 1))
+                        CheckAround(x, y - 1);
+                    break;
+                case 1:
+                    if (IsSameColor(x, y + 1))
+                        CheckAround(x, y + 1);
+                    break;
+                case 2:
+                    if (IsSameColor(x - 1, y))
+                        CheckAround(x - 1, y);
+                    break;
+                case 3:
+                    if (IsSameColor(x + 1, y))
+                        CheckAround(x + 1, y);
+                    break;
+
+            }
+        }
+    }
+
+    private bool IsSameColor(int pX,int pY)
+    {
+        // 범위 바깥
+        if (pX < 0 || pY < 0 || pX > 5 || pY > 6)
+            return false;
+
+        // 색깔이 같을경우 
+        if (board[pX, pY].GetBlockColor() == baseBlock.GetBlockColor())
+        {
+            if(!board[pX,pY].isChecked)
+            {
+                board[pX, pY].isChecked = true;
+                selectedBlock.Add(board[pX, pY]);
+                return true;
+            }
+        }
+        return false;
+    }
+}
